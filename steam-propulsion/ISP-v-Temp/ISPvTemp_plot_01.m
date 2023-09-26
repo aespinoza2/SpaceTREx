68f6ed5853286ed1ba97c78c0b3af8cbb85641be
Agustin <espinozaagustin42@gmail.com>
2023-09-25 18:38:22 -0700

    Update plots

--- steam-propulsion/ISP-v-Temp/ISPvTemp_plot_01.m
+++ steam-propulsion/ISP-v-Temp/ISPvTemp_plot_01.m
@@ -1,1 +1,49 @@
-% sdlj;dsf
+% Import mole fractions from mole fractions CSV
+mole_fract_matrix = csvread("mole_fract.csv");
+
+% Temperature array
+x_Temp = mole_fract_matrix(:,1); 
+
+% Mole Fractions
+H2O_mol_fract = mole_fract_matrix(:,2);
+HO_mol_fract = mole_fract_matrix(:,3);
+H_mol_fract = mole_fract_matrix(:,4);
+O_mol_fract = mole_fract_matrix(:,5);
+H2_mol_fract = mole_fract_matrix(:,6);
+O2_mol_fract = mole_fract_matrix(:,7);
+
+% Define the accuracy/steps
+H2O_mol_fract_q = 0:50:4500;
+HO_mol_fract_q = 0:50:4500;
+H_mol_fract_q = 0:50:4500;
+O_mol_fract_q = 0:50:4500;
+H2_mol_fract_q = 0:50:4500;
+O2_mol_fract_q = 0:50:4500;
+
+% Interpolate the function
+H2O_mol_fract_ext = interp1(x_Temp, H2O_mol_fract, H2O_mol_fract_q, 'linear');
+HO_mol_fract_ext = interp1(x_Temp, HO_mol_fract, HO_mol_fract_q, 'linear');
+H_mol_fract_ext = interp1(x_Temp, H_mol_fract, H_mol_fract_q, 'linear');
+O_mol_fract_ext = interp1(x_Temp, O_mol_fract, O_mol_fract_q, 'linear');
+H2_mol_fract_ext = interp1(x_Temp, H2_mol_fract, H2_mol_fract_q, 'linear');
+O2_mol_fract_ext = interp1(x_Temp, O2_mol_fract, O2_mol_fract_q, 'linear');
+
+% Plot the original mole fractions
+figure
+plot(x_Temp,H2O_mol_fract, 'r');
+hold on
+plot(x_Temp,HO_mol_fract, 'r');
+plot(x_Temp,H_mol_fract, 'r');
+plot(x_Temp,O_mol_fract, 'r');
+plot(x_Temp,H2_mol_fract, 'r');
+plot(x_Temp,O2_mol_fract, 'r');
+
+% Plot the interpolated points
+plot(H2O_mol_fract_q,H2O_mol_fract_ext,'*')
+plot(HO_mol_fract_q,HO_mol_fract_ext,'*')
+plot(H_mol_fract_q,H_mol_fract_ext,'*')
+plot(O_mol_fract_q,O_mol_fract_ext,'*')
+plot(H2_mol_fract_q,H2_mol_fract_ext,'*')
+plot(O2_mol_fract_q,O2_mol_fract_ext,'*')
+
+
